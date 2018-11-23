5af0986218d267f82c6d758748397732e146ad85
Dustin Morado <dustin.morado@gmail.com>
2018-11-23 17:22:10 +0000

    Big overhaul of preprocess.sh

--- src/scripts/preprocess.sh
+++ src/scripts/preprocess.sh
@@ -30,12 +30,6 @@ scratch_dir=<SCRATCH_DIR>
 # frames are located.
 frame_dir=<FRAME_DIR>
 
-# Absolute path to MCR directory for the processing.
-mcr_cache_dir=${scratch_dir}/mcr
-
-# Directory for executables
-exec_dir=XXXINSTALLATION_DIRXXX/bin
-
 ################################################################################
 #                                 EXECUTABLES                                  #
 ################################################################################
@@ -50,8 +44,8 @@ alignframes_exe=<ALIGNFRAMES_EXE>
 #ctffind_exe=$(which ctffind.exe)
 ctffind_exe=<CTFFIND_EXE>
 
-# Absolute path to the dose-filtering executable
-dose_filt_exec=${exec_dir}/dose_filter_tiltseries
+# Absolute path to the GCTF executable.
+gctf_exe=<GCTF_EXE>
 
 ################################################################################
 #                                MEMORY OPTIONS                                #
@@ -76,9 +70,9 @@ run_local=0
 # The format string for the datasets to process. The string XXXIDXXXX will be
 # replaced with the numbers specified between the range start_idx and end_idx.
 #
-# The raw sum tilt-series will have the name format ts_fmt.st, and could
-# possibly have an associated log ts_fmt.log and an extended data file
-# ts_fmt.st.mdoc
+# The raw sum tilt-series will have the name format ts_fmt.st, or ts_fmt.mrc, an
+# extended data file ts_fmt.{mrc,st}.mdoc and could possibly have an associated
+# log ts_fmt.log 
 #
 # Dose-fractionated movies of tilt-images are assumed to have the name format:
 # ${ts_fmt}_###_*.{mrc,tif} where ### is a running three-digit ID number for the
@@ -105,10 +99,6 @@ idx_fmt="%02d" # two digit zero padding e.g. 01
 #                    BEAM INDUCED MOTION CORRECTION OPTIONS                    #
 #                                                                              #
 ################################################################################
-# Determines the type of file for the dose-fractionated movie tilt-images. Set
-# to 1 to look for TIFF format frames, and 0 to default to MRC format frames.
-tiff_frames=1
-
 # Determines whether or not gain-correction needs to be done on the frames. Set
 # to 1 to apply gain-correction during motion-correction, and 0 to skip it.
 # Normally TIFF format frames will be saved with compression and will be
@@ -131,8 +121,10 @@ defects_fn=<DEFECTS_FN>
 
 # Binning to apply to the frames when calculating the alignment, if you are
 # using super-resolution you may want to change this to 2. The defaults from
-# IMOD would be 3 for counted data and 6 for super-resolution data.
-align_bin=1
+# IMOD would be 3 for counted data and 6 for super-resolution data. Multiple
+# binnings can be tested and the best one will be used to generate the final
+# sum.
+align_bin=1,2,3
 
 # Binning to apply to the final sum. This is done using Fourier cropping as in
 # MotionCorr and other similar programs. If you are using super-resolution you
@@ -142,12 +134,13 @@ sum_bin=1
 # Cutoff Frequency for the lowpass filter used in frame alignment. The unit is
 # absolute spatial frequency which goes from 0 to 0.5 relative to the pixelsize
 # of the input frames (not considering binning applied in alignment). The
-# default from IMOD is 0.06.
-filter_radius2=0.125
+# default from IMOD is 0.06. Multiple radii can be used and the best filter will
+# be selected for the actually used alignment.
+filter_radius2=0.167,0.125,0.10,0.06
 
 # Falloff for the lowpass filter used in frame alignment. Same units as above.
 # The defaults from IMOD is 0.0086.
-filter_sigma2=0.1429
+filter_sigma2=0.0086
 
 # Limit on distance to search for correlation peak in unbinned pixels. The
 # default from IMOD is 20.
@@ -179,13 +172,61 @@ extra_opts=''
 # The pixel size of the motion-corrected tilt-series in Angstroms
 apix=<APIX>
 
+# If this is set to 1, the defocus will be estimated with CTFFIND4.
+do_ctffind4=1
+
+# If this is set to 1, the defocus will be estimated with GCTF.
+do_gctf=1
+
+# If this is set to 1, the defocus will be estimated with CTFPLOTTER.
+do_ctfplotter=1
+
+# The accelerating voltage of the microscope in KeV
+voltage_kev=300.0
+
+# The spherical aberration of the microscope in mm.
+cs=2.7
+
+# The amount of amplitude contrast in the imaging system.
+ac=0.07
+
+# The size of tile to operate on
+tile_size=512
+
+# The lowest wavelength in Angstroms to allow in fitting (minimum resolution)
+min_res=30.0
+
+# The highest wavelength in Angstroms to allow in fitting (maximum resolution)
+max_res=5.0
+
+# The lowest defocus in Angstroms to scan.
+min_def=10000.0
+
+# The highest defocus in Angstroms to scan.
+max_def=60000.0
+
+# The step size in Angstroms to scan defocus.
+def_step=100.0
+
+# The amount of astigmatism to allow in angstroms
+astigmatism=1000.0
+
+# The tilt-axis angle of the tilt series. This is only needed if you are
+# estimating the CTF with ctfplotter.
+tilt_axis_angle=85.3
+
+# The expected defocus in nm to use for defocus estimation. This is only needed
+# if you are estimating the CTF with ctfplotter. A suggestion for this field is
+# to use the middle defocus of the range of defocii you collected
+expected_defocus=2500
+
 ################################################################################
 #                                                                              #
 #                            DOSE FILTERING OPTIONS                            #
 #                                                                              #
 ################################################################################
 # The dose per micrograph in Electrons per square Angstrom
-dose_rate=<DOSE_RATE>
+dose_per_tilt=<DOSE_PER_TILT>
 
 ################################################################################
 #                                                                              #
@@ -195,57 +236,58 @@ dose_rate=<DOSE_RATE>
 cd ${scratch_dir}
 
 imod_exe_dir=$(dirname ${alignframes_exe})
-
-if [[ ! -d ${mcr_cache_dir} ]]
-then
-    mkdir -p ${mcr_cache_dir}
-fi
+apix_nm=$(echo ${apix} | awk '{print $1 / 10}')
 
 for idx in $(seq ${start_idx} ${end_idx})
 do
     fmt_idx=$(printf ${idx_fmt} ${idx})
     ts=${ts_fmt/XXXIDXXXX/${fmt_idx}}
-
-    # Skip series that do not exist
-    set +e
-    ls ${frame_dir} | grep -q "${ts}"
-    has_frames=$?
-    if [[ ${has_frames} -ne 0 ]]
+    if [[ -f ${ts}.st ]];
     then
-        echo "Did not find frames for ${ts}! SKIPPING!"
+        ts_fn=${ts}.st
+        mdoc_fn=${ts}.st.mdoc
+    elif [[ -f ${ts}.mrc ]];
+    then
+        ts_fn=${ts}.mrc
+        mdoc_fn=${ts}.mrc.mdoc
+    else
+        echo "Did not find either ${ts}.st or ${ts}.mrc! SKIPPING!"
         continue
     fi
-    set -e 
 
-    if [[ ${tiff_frames} -eq 1 ]]
+    if [[ ! -f ${mdoc_fn} ]];
     then
-        frame_suffix=tif
-        frame_fmt=${ts}_???_*.tif
-        test_frame=$(ls ${frame_dir}/${ts}_001_*.tif)
-    else
-        frame_suffix=mrc
-        frame_fmt=${ts}_???_*0.mrc
-        test_frame=$(ls ${frame_dir}/${ts}_001_*0.mrc)
+        echo "Did not find associated mdoc for ${ts_fn}! SKIPPING!"
+        continue
     fi
-
-    nimg=$(ls ${frame_dir}/${frame_fmt} | wc -l)
-    dose_field=$(echo ${test_frame} | grep -o '_' | wc -l)
-    angle_field=$((dose_field + 1))
 
     if [[ ! -d ${ts} ]]
     then
         mkdir ${ts}
     fi
 
-    if [[ -f ${ts}.st ]]
+    if [[ ! -d ${ts}/alignframes ]]
     then
-        mv -n ${ts}.st ${ts}/.
+        mkdir ${ts}/alignframes
     fi
 
-    if [[ -f ${ts}.st.mdoc ]]
+    if [[ ${do_ctffind4} -eq 1 && ! -d ${ts}/ctffind4 ]]
     then
-        mv -n ${ts}.st.mdoc ${ts}/.
+        mkdir ${ts}/ctffind4
     fi
+
+    if [[ ${do_gctf} -eq 1 && ! -d ${ts}/gctf ]]
+    then
+        mkdir ${ts}/gctf
+    fi
+
+    if [[ ${do_ctfplotter} -eq 1 && ! -d ${ts}/ctfplotter ]]
+    then
+        mkdir ${ts}/ctfplotter
+    fi
+
+    mv -n ${ts_fn} ${ts}/.
+    mv -n ${mdoc_fn} ${ts}/.
 
     if [[ -f ${ts}.log ]]
     then
@@ -264,161 +306,282 @@ do
 set +o noclobber
 set -e
 echo \${HOSTNAME}
-for frame in ${frame_dir}/${frame_fmt}
-do
-    output_frame=\${frame/%\\.${frame_suffix}/_aligned.mrc}
-    if [[ -f \${output_frame} || -f ${ts}/${ts}_aligned.st ]]
-    then
-        continue
-    fi
-    
+if [[ ! -f ${ts}/${ts}_aligned.st ]]
+then
     ${alignframes_exe} \\
-        -InputFile \${frame} \\
+        -MetadataFile ${ts}/${mdoc_fn} \\
+        -AdjustAndWriteMdoc \\
+        -PathToFramesInMdoc ${frame_dir} \\
         -PairwiseFrames -1 \\
-        -AlignAndSumBinning ${align_bin},${sum_bin} \\
-        -FilterRadius2 ${filter_radius2} \\
+        -AlignAndSumBinning 1,${sum_bin} \\
+        -TestBinnings ${align_bin} \\
+        -VaryFilter ${filter_radius2} \\
         -FilterSigma2 ${filter_sigma2} \\
         -ShiftLimit ${shift_limit} \\
+        -TransformExtension aligned.xf \\
+        -FRCOutputFile ${ts}/alignframes/${ts}_aligned_FRC.txt \\
+        -PlottableShiftFile ${ts}/alignframes/${ts}_aligned_plot.txt \\
+        -DebugOutput 3 \\
 PREPROC
 
-    if [[ ${do_gain_correction} -eq 1 ]]
-    then
-        cat<<PREPROC>>preprocess_${fmt_idx}.sh
+if [[ ${do_gain_correction} -eq 1 ]]
+then
+    cat<<PREPROC>>preprocess_${fmt_idx}.sh
         -GainReferenceFile ${gainref_fn} \\
         -CameraDefectFile ${defects_fn} \\
         -RotationAndFlip -1 \\
         -ImagesAreBinned 1.0 \\
 PREPROC
-    fi
+fi
 
-    if [[ ${do_refinement} -eq 1 ]]
-    then
-        cat<<PREPROC>>preprocess_${fmt_idx}.sh
+if [[ ${do_refinement} -eq 1 ]]
+then
+    cat<<PREPROC>>preprocess_${fmt_idx}.sh
         -RefineAlignment ${refine_iterations} \\
         -RefineRadius2 ${refine_radius2} \\
         -StopIterationsAtShift ${refine_shift_stop} \\
 PREPROC
-    fi
+fi
 
-    if [[ -z ${extra_opts//} ]]
-    then
-        cat<<PREPROC>>preprocess_${fmt_idx}.sh
+if [[ -n ${extra_opts} ]]
+then
+    cat<<PREPROC>>preprocess_${fmt_idx}.sh
         ${extra_opts} \\
 PREPROC
-    fi
+fi
 
     cat<<PREPROC>>preprocess_${fmt_idx}.sh
-        -OutputImageFile \${output_frame}
-done
-
-if [[ ! -f ${ts}/${ts}_aligned.st ]]
-then
-    find ${frame_dir} -name "${ts}_???_*_aligned.mrc" | \\
-    sort -t_ -n -k ${angle_field},${angle_field} | \\
-    awk -v nimg=${nimg} '
-        BEGIN {
-            print nimg;
-        }
-        {
-            printf("%s\\n0\\n", \$0);
-        }
-    ' > ${ts}_filelistin.txt
-    ${imod_exe_dir}/newstack -q -filein ${ts}_filelistin.txt \\
-        -output ${ts}/${ts}_aligned.st
-    rm ${ts}_filelistin.txt
-fi
-
-if [[ ! -f ${ts}/${ts}_dose-filt.rawtlt ]]
-then
-    find ${frame_dir} -name "${frame_fmt}" | \\
-    xargs -n 1 -I {} basename {} .${frame_suffix} | \\
-    sort -t_ -n -k ${angle_field},${angle_field} | \\
-    awk -F_ -v field=${angle_field} '
-        {
-            tilt_angle = \$field + 0.0;
-            printf("%f\n", tilt_angle);
-        }
-    ' > ${ts}/${ts}_dose-filt.rawtlt
-fi
-
-if [[ ! -f ${ts}/${ts}_dose_list.csv ]]
-then
-    find ${frame_dir} -name "${frame_fmt}" | \\
-    sort -t_ -n -k ${dose_field},${dose_field} | \\
-    awk -F_ -v dose_rate=${dose_rate} -v field=${angle_field} '
-        BEGIN {
-            dose = 0.0;
-        }
-        {
-            printf("%d\t%f\n", \$field + 0, dose);
-            dose += dose_rate;
-        }
-    ' |\\
-    sort -n -k1,1 |\\
-    cut -f 2 > ${ts}/${ts}_dose_list.csv
-fi
-
-if [[ -f \$(ls ${frame_dir}/${ts}_001_*_aligned.mrc) ]]
-then
-    rm ${frame_dir}/${ts}_???_*_aligned.mrc
-fi
-
-if [[ ! -f ${ts}/${ts}_output.ctf ]]
-then
-    ln -sv ${ts}_aligned.st ${ts}/${ts}_aligned.mrc
-    ${ctffind_exe}<<CTFFINDCARD
-${ts}/${ts}_aligned.mrc
-no
-${ts}/${ts}_output.ctf
-${apix}
-300.0
-2.70
-0.07
-512
-50.0
-4.0
-10000.0
-80000.0
-500.0
-no
-yes
-yes
-1000.0
-no
-no
-CTFFINDCARD
-    rm ${ts}/${ts}_aligned.mrc
+        -OutputImageFile ${ts}/${ts}_aligned.st | \\
+    tee ${ts}/alignframes/${ts}_aligned.out
+    newstack \\
+        -ReorderByTiltAngle 1 \\
+        -UseMdocFiles \\
+        ${ts}/${ts}_aligned.st \\
+        ${ts}/${ts}_aligned.st
+    rm ${ts}/${ts}_aligned.st\~ ${ts}/${ts}_aligned.st.mdoc\~
 fi
 
 if [[ ! -f ${ts}/${ts}_dose-filt.st ]]
 then
-    ldpath=XXXMCR_DIRXXX/runtime/glnxa64
-    ldpath=\${ldpath}:XXXMCR_DIRXXX/bin/glnxa64
-    ldpath=\${ldpath}:XXXMCR_DIRXXX/sys/os/glnxa64
-    ldpath=\${ldpath}:XXXMCR_DIRXXX/sys/opengl/lib/glnxa64
-    export LD_LIBRARY_PATH=\${ldpath}
-    mcr_cache_dir=${mcr_cache_dir}/dose_filter_${fmt_idx}
-    if [[ ! -d \${mcr_cache_dir} ]]
-    then
-        mkdir -p \${mcr_cache_dir}
-    else
-        rm -rf \${mcr_cache_dir}
-        mkdir -p \${mcr_cache_dir}
-    fi
-    export MCR_CACHE_ROOT=\${mcr_cache_dir}
-    time ${dose_filt_exec} \\
-        ${ts}/${ts}_aligned.st \\
+    ${alignframes_exe} \\
+        -MetadataFile ${ts}/${mdoc_fn} \\
+        -AdjustAndWriteMdoc \\
+        -PathToFramesInMdoc ${frame_dir} \\
+        -PairwiseFrames -1 \\
+        -AlignAndSumBinning 1,${sum_bin} \\
+        -TestBinnings ${align_bin} \\
+        -VaryFilter ${filter_radius2} \\
+        -FilterSigma2 ${filter_sigma2} \\
+        -ShiftLimit ${shift_limit} \\
+        -TransformExtension dose-filt.xf \\
+        -FRCOutputFile ${ts}/alignframes/${ts}_dose-filt_FRC.txt \\
+        -PlottableShiftFile ${ts}/alignframes/${ts}_dose-filt_plot.txt \\
+        -DebugOutput 3 \\
+        -PixelSize ${apix_nm} \\
+        -FixedTotalDose ${dose_per_tilt} \\
+PREPROC
+
+if [[ ${do_gain_correction} -eq 1 ]]
+then
+    cat<<PREPROC>>preprocess_${fmt_idx}.sh
+        -GainReferenceFile ${gainref_fn} \\
+        -CameraDefectFile ${defects_fn} \\
+        -RotationAndFlip -1 \\
+        -ImagesAreBinned 1.0 \\
+PREPROC
+fi
+
+if [[ ${do_refinement} -eq 1 ]]
+then
+    cat<<PREPROC>>preprocess_${fmt_idx}.sh
+        -RefineAlignment ${refine_iterations} \\
+        -RefineRadius2 ${refine_radius2} \\
+        -StopIterationsAtShift ${refine_shift_stop} \\
+PREPROC
+fi
+
+if [[ -n ${extra_opts} ]]
+then
+    cat<<PREPROC>>preprocess_${fmt_idx}.sh
+        ${extra_opts} \\
+PREPROC
+fi
+
+    cat<<PREPROC>>preprocess_${fmt_idx}.sh
+        -OutputImageFile ${ts}/${ts}_dose-filt.st | \\
+    tee ${ts}/alignframes/${ts}_dose-filt.out
+
+    extracttilts ${ts}/${ts}_dose-filt.st | \\
+    awk -vd=${dose_per_tilt} 'BEGIN { dose = 0 }
+        { printf("%6.2f\t%10.4f\n", $1, dose); dose += d }' | \\
+    sort -n -k 1,1 | awk '{print $2}' > ${ts}/${ts}_dose_list.csv
+    newstack \\
+        -ReorderByTiltAngle 1 \\
+        -UseMdocFiles \\
         ${ts}/${ts}_dose-filt.st \\
-        ${ts}/${ts}_dose_list.csv
-    rm -rf \${mcr_cache_dir}
+        ${ts}/${ts}_dose-filt.st
+    rm ${ts}/${ts}_dose-filt.st\~ ${ts}/${ts}_dose-filt.st.mdoc\~
+fi
+
+if [[ ${do_ctffind4} -eq 1 && ! -f ${ts}/ctffind4/${ts}_output.ctf ]]
+then
+    if [[ ! -f ${ts}/${ts}_aligned.mrc ]]
+    then
+        ln -sv ${ts}_aligned.st ${ts}/${ts}_aligned.mrc
+    fi
+
+    ${ctffind_exe}<<CTFFINDCARD
+${ts}/${ts}_aligned.mrc
+no
+${ts}/ctffind4/${ts}_output.ctf
+${apix}
+${voltage_kev}
+${cs}
+${ac}
+${tile_size}
+${min_res}
+${max_res}
+${min_def}
+${max_def}
+${def_step}
+no
+no
+yes
+${astigmatism}
+no
+yes
+no
+CTFFINDCARD
+
+    nz=\$(header -size ${ts}/${ts}_aligned.mrc | awk '{print $3}')
+    mz=\$((nz / 2 + 1))
+    def_avg=\$(grep "^\${mz}" ${ts}/ctffind4/${ts}_output.txt | \\
+       awk '{ printf("%d", ($2 + $3) / 2) }')
+    def_lo=\$((def_avg - 5000))
+    def_hi=\$((def_avg + 5000))
+
+    ${ctffind_exe}<<CTFFINDCARD
+${ts}/${ts}_aligned.mrc
+no
+${ts}/ctffind4/${ts}_output.ctf
+${apix}
+${voltage_kev}
+${cs}
+${ac}
+${tile_size}
+${min_res}
+${max_res}
+\${def_lo}
+\${def_hi}
+100.0
+no
+yes
+yes
+${astigmatism}
+no
+yes
+no
+CTFFINDCARD
+
+    rm ${ts}/${ts}_aligned.mrc
+fi
+
+if [[ ${do_gctf} -eq 1 && ! -f ${ts}/gctf/${ts}_output.ctf ]]
+then
+    newstack \\
+        -NumberedFromOne \\
+        -SplitStartingNumber 1 \\
+        ${ts}/${ts}_aligned.st \\
+        ${ts}/gctf/${ts}_aligned
+    num_tilts=\$(ls ${ts}/gctf/${ts}_aligned.[0-9]* | wc -l)
+    for tilt_idx in \$(seq 1 \${num_tilts})
+    do
+        if [[ \${num_tilts} -le 10 ]]
+        then
+            fmt_tilt_idx=\${tilt_idx}
+        elif [[ \${num_tilts} -le 100 ]]
+        then
+            fmt_tilt_idx=\$(printf "%02d" \${tilt_idx})
+        else
+            fmt_tilt_idx=\$(printf "%02d" \${tilt_idx})
+        fi
+        mv ${ts}/gctf/${ts}_aligned.\${fmt_tilt_idx} \\
+            ${ts}/gctf/${ts}_aligned_\${fmt_tilt_idx}.mrc
+    done
+    ${gctf_exe} \\
+        --apix ${apix} \\
+        --ac ${ac} \\
+        --kV ${voltage_kev} \\
+        --defH ${max_def} \\
+        --defS ${def_step} \\
+        --astm ${astigmatism} \\
+        --resL ${min_res} \\
+        --resH ${max_res} \\
+        --boxsize ${tile_size} \\
+        --do_EPA \\
+        ${ts}/gctf/${ts}_aligned_*.mrc
+    echo \${num_tilts} > ${ts}/gctf/${ts}_output_filein.txt
+    for tilt_idx in \$(seq 1 \${num_tilts})
+    do
+        if [[ \${num_tilts} -le 10 ]]
+        then
+            fmt_tilt_idx=\${tilt_idx}
+        elif [[ \${num_tilts} -le 100 ]]
+        then
+            fmt_tilt_idx=\$(printf "%02d" \${tilt_idx})
+        else
+            fmt_tilt_idx=\$(printf "%02d" \${tilt_idx})
+        fi
+        echo ${ts}/gctf/${ts}_aligned_\${fmt_tilt_idx}.ctf >> \\
+            ${ts}/gctf/${ts}_output_filein.txt
+        echo 0 >> ${ts}/gctf/${ts}_output_filein.txt
+    done
+    newstack -filein ${ts}/gctf/${ts}_output_filein.txt \\
+        ${ts}/gctf/${ts}_output.ctf
+    rm ${ts}/gctf/${ts}_output_filein.txt
+    cat ${ts}/gctf/${ts}_aligned_*_EPA.log > ${ts}/gctf/${ts}_aligned_EPA.log
+    rm ${ts}/gctf/${ts}_aligned_*_EPA.log
+    cat ${ts}/gctf/${ts}_aligned_*_gctf.log > ${ts}/gctf/${ts}_aligned_gctf.log
+    rm ${ts}/gctf/${ts}_aligned_*_gctf.log
+    rm ${ts}/gctf/${ts}_aligned_*.ctf
+    rm ${ts}/gctf/${ts}_aligned_*.mrc
+    mv micrographs_all_gctf.star ${ts}/gctf/${ts}_output.star
+fi
+
+if [[ ${do_ctfplotter} -eq 1 && ! -f ${ts}/ctfplotter/${ts}_output.txt ]]
+then
+    extracttilts ${ts}/${ts}_aligned.st ${ts}/ctfplotter/${ts}_aligned.rawtlt
+    asf_def_lo=$(echo '' | awk -vrl=${min_res} -vpx=${apix} \\
+        '{printf("%f", px/dl)}')
+    asf_def_hi=$(echo '' | awk -vrh=${max_res} -vpx=${apix} \\
+        '{printf("%f", px/dh)}')
+    int_volt=$(printf "%.0f" ${voltage_kev})
+    ctfplotter \\
+        -InputStack ${ts}/${ts}_aligned.st \\
+        -AngleFile ${ts}/ctfplotter/${ts}_aligned.rawtlt \\
+        -AxisAngle ${tilt_axis_angle} \\
+        -DefocusFile ${ts}/ctfplotter/${ts}_output.txt \\
+        -PixelSize ${apix_nm} \\
+        -Voltage \${int_volt} \\
+        -SphericalAberration ${cs} \\
+        -AmplitudeContrast ${ac} \\
+        -ExpectedDefocus ${expected_defocus} \\
+        -AutoFitRangeAndStep 0,0 \\
+        -FrequencyRangeToFit \${asf_def_lo},\${asf_def_hi} \\
+        -VaryExponentInFit \\
+        -BaselineFittingOrder 4 \\
+        -TileSize ${tile_size} \\
+        -FindAstigPhaseCuton 1,0,0 \\
+        -SaveAndExit
 fi
 PREPROC
 
     if [[ ${run_local} -eq 1 ]]
     then
         chmod u+x preprocess_${fmt_idx}.sh
-        ./preprocess_${fmt_idx}.sh
+        #./preprocess_${fmt_idx}.sh
     else
-        qsub preprocess_${fmt_idx}.sh
+        :
+        #qsub preprocess_${fmt_idx}.sh
     fi
 done
